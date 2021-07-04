343e187f9786fb4ff4a0b36a7da436b12165f13d
sunsongsong <sunsongsong@teddymobile.cn>
2021-07-04 09:17:11 +0800

    添加谷粒商城1

--- gulimall-product/src/test/java/com/mandian/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/mandian/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,28 @@
 package com.mandian.gulimall.product;
 
+import com.mandian.gulimall.product.entity.BrandEntity;
+import com.mandian.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void test(){
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandEntity.setDescript("test");
+        brandService.save(brandEntity);
+        System.out.println("保存成功。。。");
     }
 
 }
